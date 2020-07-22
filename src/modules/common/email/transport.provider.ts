d3ea0331a4671ec834497be1174cd5e2b6285927
fuwei <454575238@qq.com>
2020-07-22 20:36:52 +0800

    fix: smtpTransport module lint error

--- src/modules/common/email/transport.provider.ts
+++ src/modules/common/email/transport.provider.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 
 import { Transporter, createTransport } from 'nodemailer'
-import smtpTransport from 'nodemailer-smtp-transport'
+import * as smtpTransport from 'nodemailer-smtp-transport'
 
 @Injectable()
 export class TransportProvider {
